3de6a4ecdb0e6e992a227bdc5f2012e136634991
JedidiahBertram <jedidiah.bertram@gmail.com>
2018-04-01 15:51:02 -0500

    adds one solution

--- solutions.js
+++ solutions.js
@@ -204,3 +204,13 @@
  //   return count;
  //
  // }
+ // 
+ //  Title: Count by X
+ //
+ //  Solution: function countBy(x, n) {
+ //   var z = [];
+ //   for(i = 1; i <= n; i++){
+ //   z.push(x * i);
+ //   }
+ //   return z;
+ // }
